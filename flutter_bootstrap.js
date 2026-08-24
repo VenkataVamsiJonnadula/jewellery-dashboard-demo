aca5af7574bdf97a785b252a1250d2bd333f01ae
VenkataVamsiJonnadula <venkat.nov20@gmail.com>
2026-08-24 06:03:33 +0530

    Update asset paths in flutter_bootstrap.js and index.html for correct resource loading

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,7 +38,7 @@ _flutter.buildConfig = {"engineRevision":"59aa584fdf100e6c78c785d8a5b565d1de4b48
 
 _flutter.loader.load({
   config: {
-    fontFallbackBaseUrl: '/assets/'
+    fontFallbackBaseUrl: 'assets/'
   },
   onEntrypointLoaded: async function(engineInitializer) {
     const appRunner = await engineInitializer.initializeEngine();
